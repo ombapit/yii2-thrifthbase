d43eb1e087958407df87d443f42d477f9bad19fa
David Suwandi <davidsuwandi@gmail.com>
2015-10-06 16:50:37 +0700

    test module

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ombapit\thrifthbase;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\runtime\tmpextensions\yii2thrifthbase\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ombapit\thrifthbase\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
